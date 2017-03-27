d111fcc47583669766777c217615cd2e080f6cec
selva <selvaraj.antonyraj@sc.com>
2017-03-27 12:14:58 +0800

    Bug fix

--- index.js
+++ index.js
@@ -2,5 +2,24 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-swiper'
+  name: 'ember-swiper',
+  init: function(name) {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    var assets_path = require('path').join('dist', 'js', 'swiper.js');
+    this.treePaths['vendor'] = require.resolve('swiper').replace(assets_path, '');
+  },
+  included: function(app) {
+  	var target;
+  	while (app.app) {
+  		app = app.app
+  	}
+    target = app;
+
+    var options = target.options.swiperOptions || {enabled: true};
+    if (options.enabled) {
+        target.import('vendor/dist/js/swiper.min.js');
+        target.import('vendor/dist/css/swiper.min.css');
+    }
+  }
 };
